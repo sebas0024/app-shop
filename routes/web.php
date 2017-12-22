b1eb30016cb6897b3e4582a295fbd0c86090d6fa
juan sebastian <jsorrego46@misena.edu.co>
2017-12-21 21:35:28 -0500

    trabajando en App Shop

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ Route::get('/', 'TestController@welcome');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin/products', 'ProductController@index');
+Route::get('/admin/products/create', 'ProductController@create');
+Route::post('/admin/products', 'ProductController@store');
+Route::get('/admin/products/{id}/edit', 'ProductController@edit');
+Route::post('/admin/products/{id}/edit', 'ProductController@update');
